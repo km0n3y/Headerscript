b480cfa91b7a67e3705693d6ab4446bc980b5385
km0n3y <kristy.patullo@gmail.com>
2016-05-18 22:02:40 -0700

    Add files via upload

---
+++ Headerscript/Headerscript.sh
@@ -0,0 +1,2 @@
+curl --trace-ascii originserver.txt --trace-time --resolve kpatullo.com:192.241.203.207 http://kpatullo.com
+curl --trace-ascii cloudflare.txt --trace-time --resolve kpatullo.com  http://kpatullo.com
